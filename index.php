6c05a35474334accf96170ef056b980b93b84855
Mikhail0801 <mishshanya220@gmail.com>
2024-02-11 15:23:03 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+    <title>Авторизация и Регистрация</title>
+</head>
+<body>
+    <!--Форма авторизации-->
+
+<form action="" method ="">
+   <label>Логин</label>
+   <input type="text" placeholder="Введите свой логин">
+   <label>Пароль</label>
+   <input type="password" placeholder="Введите пароль">
+   <button>Войти</button>
+   <p>
+    У вас нет аккаунта? - <a href="/register.php">Зарегистрируйтесь</a>!
+   </p>
+</form>
+
+
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+
+    <!--Форма авторизации-->
+
+<form action="" method ="">
+   <label>ФИО</label>
+   <input type="text" placeholder="Введите свое полное имя">
+   <label>Логин</label>
+   <input type="text" placeholder="Введите свой логин">
+   <label>Почта</label>
+   <input type="email" placeholder="Введите адрес электронной почты">
+   <label>Изображение профиля</label>
+   <input type="file">
+   <label>Пароль</label>
+   <input type="password" placeholder="Введите пароль">
+   <label>Подтверждение пароля</label>
+   <input type="password" placeholder="Подтвердите пароль">
+
+
+
+   <button>Войти</button>
+   <p>
+    У вас уже есть аккаунт? - <a href="/">Авторизируйтесь</a>!
+   </p>
+</form>
